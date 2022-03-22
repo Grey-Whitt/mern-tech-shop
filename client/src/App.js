3b919782381abf7c467ce04b5e7558603f0b19bf
greyw <greywhitt@gmail.com>
2022-03-22 14:20:39 -0500

    added first components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to Tech Shop</h1>
-    </div>
-  );
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Tech Shop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </>
+  )
 }
 
-export default App;
+export default App
