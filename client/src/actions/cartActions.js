40115aa8c3eaf832ce399c51deaf83555fb9dc6a
greyw <greywhitt@gmail.com>
2022-03-24 23:32:14 -0500

    clicking add to cart now stores cart items in state

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -15,5 +15,6 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       qty,
     },
   })
+
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useParams, useSearchParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
 const Cart = () => {
+  const params = useParams()
+  const [searchParams] = useSearchParams()
+
+  const productId = params.id
+
+  const qty = searchParams.get('qty') || 1
+  //const qty = searchParams.get('qty') ? searchParams.get('qty') : 1
+
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+
+  console.log(cartItems)
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [(dispatch, productId, qty)])
+
   return <div>Cart</div>
 }
 
