b2d60f6ea4804187e46829b177308f0462ee0bda
greyw <greywhitt@gmail.com>
2022-03-25 12:43:59 -0500

    added cart screen and remove from cart functionality

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
             <Route path='/' element={<HomeScreen />} />
             <Route path='/products/:id' element={<ProductScreen />} />
             <Route path='/cart/:id' element={<CartScreen />} />
-            <Route path='/cart/' element={<CartScreen />} />
+            <Route path='/cart' element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
