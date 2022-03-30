b895d7ae96f50814b56bf7c0ad05f154fe1f35f5
greyw <greywhitt@gmail.com>
2022-03-29 19:19:13 -0500

    orders now shown on profile and state is reset when logged out

--- client/src/screens/OrderScreen.js
+++ client/src/screens/OrderScreen.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { useNavigate, Link, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import axios from 'axios'
 import { PayPalButton } from 'react-paypal-button-v2'
-import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
@@ -15,13 +15,12 @@ const OrderScreen = () => {
   const [sdkReady, setSdkReady] = useState(false)
 
   const dispatch = useDispatch()
-  const navigate = useNavigate()
 
   const orderDetails = useSelector((state) => state.orderDetails)
   const { order, loading, error } = orderDetails
 
   const orderPay = useSelector((state) => state.orderPay)
-  const { success, loading: loadingPay, success: successPay } = orderPay
+  const { loading: loadingPay, success: successPay } = orderPay
 
   if (!loading) {
     order.itemsPrice = order.orderItems
