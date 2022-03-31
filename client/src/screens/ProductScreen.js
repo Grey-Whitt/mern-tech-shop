58c5baaec55c76bcb78842e222c2d2ab5371d18f
greyw <greywhitt@gmail.com>
2022-03-31 18:18:35 -0500

    added custom page titles with react-helmet

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom'
 import Rating from '../components/Rating'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import Meta from '../components/Meta'
 import {
   listProductsDetails,
   createProductReview,
@@ -68,6 +69,7 @@ const ProductScreen = () => {
         <Message variant='danger'>{error}</Message>
       ) : (
         <>
+          <Meta title={product.name} />
           <Row>
             <Col md={6}>
               <Image src={product.image} alt={product.name} fluid />
